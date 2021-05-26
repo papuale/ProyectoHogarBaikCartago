486f50be3fb7329f91bde7606af92d1f2263ed2a
MAURO ARCE <marce80156@ufide.ac.cr>
2021-05-26 11:46:57 -0600

    Insert

--- hogarbaik/Controllers/NinosController.cs
+++ hogarbaik/Controllers/NinosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using hogarbaik.BD;
+using hogarbaik.Entidades;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,7 @@ namespace hogarbaik.Controllers
 {
     public class NinosController : Controller
     {
-        public IActionResult NinosActivos()
+        public ActionResult NinosActivos()
         {
             return View();
         }
@@ -36,6 +38,57 @@ namespace hogarbaik.Controllers
         public IActionResult Medico()
         {
             return View();
+        }
+   
+        [HttpPost]
+        public IActionResult RegistroNino(clsNino infoNino)
+        {
+            String fecha = "2005-05-25";
+            using (var BD = new bdhogarbaikContext())
+            {
+                InformacionNino Ninobd = new InformacionNino();
+                Ninobd.PkCedula = infoNino.PkCedula;
+                Ninobd.Nombre = infoNino.Nombre;
+                Ninobd.PrimerApellido = infoNino.PrimerApellido;
+                Ninobd.SegundoApellido = infoNino.SegundoApellido;
+                Ninobd.FechaNacimiento = DateTime.Parse(fecha);
+                Ninobd.Edad = 5;
+                Ninobd.ArchivoConstancia = "Constancia.pf";
+                Ninobd.Estado = "Activo";
+                Ninobd.FechaIngreso = DateTime.Parse(fecha);
+                Ninobd.EncargadoIngreso = "Jorge Carrascal";
+                Ninobd.CedulaEncargado = 302560879;
+                Ninobd.MotivoIngreso = "Abandono";
+                Ninobd.UbicacionOficina = "Cartago, Costa Rica";
+                Ninobd.TelefonoOficina = 25528214;
+                Ninobd.CorreoOficina = "sad@gmail.com";
+                Ninobd.ArchivoIngreso = "Ingreso.pdf";
+                Ninobd.FechaEgreso = DateTime.Parse(fecha);
+                Ninobd.CedulaEncargadoEgreso = 123456789;
+                Ninobd.EncargadoEgreso = "Bryan Ruiz";
+                Ninobd.DetalleEgreso = "Adopcion";
+                Ninobd.DestinoEgreso = "Casa familiar";
+                Ninobd.ContactoEgreso = "25849631";
+                Ninobd.ArchivoEgreso = "Egreso.pdf";
+                //Ninobd.IdCodigoEducativo = 5;
+                //Ninobd.IdCodigoMedico = 5;
+
+
+
+                BD.Add(Ninobd);
+                BD.SaveChanges();
+
+
+            }
+                return RegistroNino();
+
+        }
+
+        public ActionResult RegistroNino()
+        {
+            return View("NinosActivos");
+
+
         }
     }
 }
---
+++ hogarbaik/Entidades/clsNino.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hogarbaik.Entidades
+{
+    public class clsNino
+    {
+        public int PkCedula { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public string ArchivoConstancia { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string EncargadoIngreso { get; set; }
+        public int CedulaEncargado { get; set; }
+        public string MotivoIngreso { get; set; }
+        public string UbicacionOficina { get; set; }
+        public int TelefonoOficina { get; set; }
+        public string CorreoOficina { get; set; }
+        public string ArchivoIngreso { get; set; }
+        public DateTime? FechaEgreso { get; set; }
+        public string EncargadoEgreso { get; set; }
+        public int CedulaEncargadoEgreso { get; set; }
+        public string DetalleEgreso { get; set; }
+        public string DestinoEgreso { get; set; }
+        public string ContactoEgreso { get; set; }
+        public string ArchivoEgreso { get; set; }
+        public int? IdCodigoEducativo { get; set; }
+        public int? IdCodigoMedico { get; set; }
+    }
+}
